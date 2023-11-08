6f7945f7710085e3a4e229c60f039a34b972b119
IceWolf9/11 <110422407+Icewolf911@users.noreply.github.com>
2023-11-08 21:56:08 +0200

    Update DataHandler.cs

--- Project1 PRG282/DataAccess/DataHandler.cs
+++ Project1 PRG282/DataAccess/DataHandler.cs
@@ -12,15 +12,78 @@ namespace Project1_PRG282.DataAccess
 
         public void createStudent()
         {
-            String query = $"INSERT INTO Student VALUES ()";
+            String query = $"INSERT INTO Student VALUES ('{student.Studentnumber}', '{student.Name}', '{student.Surname}', '{student.StudentImage}', '{student.DOB1}', '{student.Gender}'," +
+                $" '{student.Phone}', '{student.Address}', '{student.ModuleCode}' )";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connect))
+                {
+                    conn.Open();
+
+                    using(SqlCommand command = new SqlCommand(query, conn))
+                    {
+                        command.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                    MessageBox.Show("Created Student");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
         public void updateStudent()
         {
+            string query = $"UPDATE Student SET StudentNumber = {student.Studentnumber}', Name = '{student.Name}', Surname = '{student.Surname}', " +
+    $"StudentImage = '{student.StudentImage}', DOB = '{student.DOB1}', Gender = '{student.Gender}'," +
+    $" Phone = '{student.Phone}', Address = '{student.Address}', ModuleCode = '{student.ModuleCode}'";
 
+try
+{
+    using (SqlConnection conn = new SqlConnection(connect))
+    {
+        conn.Open();
+
+        using (SqlCommand command = new SqlCommand(query, conn))
+        {
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        MessageBox.Show("Student Updated");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
         }
         public void deleteStudent()
         {
+            string query = $"Delete from Student Where StudentNumber = '{StudentNumber}'";
 
+try
+{
+    using (SqlConnection conn = new SqlConnection(connect))
+    {
+        conn.Open();
+
+        using (SqlCommand command = new SqlCommand(query, conn))
+        {
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        MessageBox.Show($"Data for student {StudentNumber} deleted successfully");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
         }
         public void searchStudent()
         {
